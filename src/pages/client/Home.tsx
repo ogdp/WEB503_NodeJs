9fc87228dd31ee951214fe5a7df904932a454c96
MinhDuc <ogdp.vn@gmail.com>
2023-04-09 17:41:59 +0700

    uninstall socket

--- src/pages/client/Home.tsx
+++ src/pages/client/Home.tsx
@@ -1,16 +1,6 @@
 import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import { Outlet } from "react-router-dom";
-import { io } from "socket.io-client";
-
-const socket = io("http://localhost:8080"); // Thay đổi URL của server tương ứng với dự án của bạn
-// Gửi một sự kiện đến server
-socket.emit("message", "Hello from client");
-
-// Nhận các sự kiện từ server
-socket.on("message", (data) => {
-  console.log(`Received message from server: ${data}`);
-});
 
 const Homepage = () => {
   return (
